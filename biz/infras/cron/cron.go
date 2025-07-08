186891ea56c7642f88a827b6ad2d806027581ece
kcersing <wt4@live.cn>
2025-07-09 00:47:09 +0800

    问卷

--- biz/infras/cron/cron.go
+++ biz/infras/cron/cron.go
@@ -13,11 +13,13 @@ func run() {
 
 	tickerSecond_5 := time.NewTicker(5 * time.Second)
 	tickerSecond_30 := time.NewTicker(30 * time.Second)
+	tickerMinute_30 := time.NewTicker(30 * time.Minute)
+
 	tickerHour_6 := time.NewTicker(6 * time.Hour)
 
 	go func() {
 		//首次进入调用
-		setResponseAnswersCount()
+
 	}()
 	for {
 		select {
@@ -25,9 +27,10 @@ func run() {
 
 		case <-tickerSecond_30.C:
 
+		case <-tickerMinute_30.C:
+			setResponseAnswersCount()
 		case <-tickerHour_6.C:
 
-			setResponseAnswersCount()
 		}
 
 	}
